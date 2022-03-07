59eb040be70dda91c9983c324576fbffa823d432
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-07 09:45:54 -0800

    Added authentication components.

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\RecipientController;
-use App\Http\Controllers\OrganizationController;
-use App\Http\Controllers\AwardController;
-use App\Http\Controllers\CommunityController;
-use App\Http\Controllers\PecsfController;
-
-use App\Models\Recipient;
-use App\Models\Organization;
-use App\Models\Award;
-use App\Models\Community;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,11 +14,7 @@ use App\Models\Community;
 |
 */
 
-if (App::environment('production')) {
-    URL::forceScheme('https');
-}
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
