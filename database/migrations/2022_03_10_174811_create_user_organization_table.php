e62e02fb148770d18af18418e72351a953709d40
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-10 10:03:08 -0800

    Added migration for many-to-many relationship for users/orgs, removed org property on user table.

--- database/migrations/2022_03_10_174811_create_user_organization_table.php
+++ database/migrations/2022_03_10_174811_create_user_organization_table.php
@@ -13,16 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_organization', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('guid')->nullable();
-            $table->string('idir');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('organization_id')->constrained();
         });
     }
 
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_organization');
     }
 };
