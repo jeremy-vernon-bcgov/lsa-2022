98b2f0848f76c84b833c0c27437dd715d06ca102
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-11 12:51:51 -0800

    Fleshed out Recipient factory.

--- database/factories/RecipientFactory.php
+++ database/factories/RecipientFactory.php
@@ -17,12 +17,32 @@ class RecipientFactory extends Factory
     public function definition()
     {
         return [
-          'first_name' => $this->faker->name(),
-          'last_name' => $this->faker->name(),
-          'idir' => $this->faker->unique(),
-          'guid' => $this->faker->unique(),
-          'personal_email' => $this->faker->unique()->safeEmail(),
-          'organization_id' => 22,
+            'first_name'                => $this->faker->firstName(),
+            'last_name'                 => $this->faker->lastName(),
+            'idir'                      => $this->faker->userName(),
+            'employee_number'           => $this->faker->unique()->randomNumber(6, true),
+            'guid'                      => $this->faker->uuid(),
+            'organization_id'           => $this->faker->numberBetween(1, 78),
+            'branch_name'               => $this->faker->words(3, true),
+            'government_email'          => $this->faker->companyEmail(),
+            'milestones'                => $this->faker->randomElement([25, 30, 35, 40 ,45 ,50 ,55]),
+            'qualifying_year'           => $this->faker->randomElement([2019, 2020, 2021, 2022]),
+            'is_bcgeu_memmber'          => $this->faker->randomElement([true, false]),
+            'retiring_this_year'        => $this->faker->randomElement([true, false]),
+            'retirement_date'           => $this->faker->date(),
+            'government_phone_number'   => $this->faker->phoneNumber(),
+            'personal_email'            => $this->faker->email(),
+            'personal_phone_number'     => $this->faker->phoneNumber(),
+            'supervisor_first_name'     => $this->faker->firstName(),
+            'supervisor_last_name'      => $this->faker->lastName(),
+            'supervisor_email'          => $this->faker->companyEmail(),
+            'survey_participation'      => $this->faker->randomElement([true, false]),
+            'is_declared'               => $this->faker->randomElement([true, false]),
+            'ceremony_opt_out'          => $this->faker->randomElement([true, false]),
+            'admin_notes'               => $this->faker->sentence(),
+
+
+
         ];
     }
 }
