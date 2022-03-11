fc6110f293c6a2c60c82e10b82ac167632109f1e
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-10 16:28:36 -0800

    Updated PECSF charity and PECSF region seeders.

--- database/seeders/PecsfRegionSeeder.php
+++ database/seeders/PecsfRegionSeeder.php
@@ -25,7 +25,7 @@ class PecsfRegionSeeder extends Seeder
         DB::table('pecsf_regions')->insert(['id' => 6,'name' => "Central Kootenay"]);
         DB::table('pecsf_regions')->insert(['id' => 7,'name' => "Central Okanagan"]);
         DB::table('pecsf_regions')->insert(['id' => 8,'name' => "Columbia-Shuswap"]);
-        DB::table('pecsf_regions')->insert(['id' => 9,'name' => "Comox-Strathcona"]);
+        DB::table('pecsf_regions')->insert(['id' => 9,'name' => "Comox Valley"]);
         DB::table('pecsf_regions')->insert(['id' => 10,'name' => "Cowichan Valley"]);
         DB::table('pecsf_regions')->insert(['id' => 11,'name' => "East Kootenay"]);
         DB::table('pecsf_regions')->insert(['id' => 12,'name' => "Northern Rockies"]);
@@ -37,14 +37,15 @@ class PecsfRegionSeeder extends Seeder
         DB::table('pecsf_regions')->insert(['id' => 18,'name' => "North Okanagan"]);
         DB::table('pecsf_regions')->insert(['id' => 19,'name' => "Okanagan-Similkameen"]);
         DB::table('pecsf_regions')->insert(['id' => 20,'name' => "Peace River"]);
-        DB::table('pecsf_regions')->insert(['id' => 21,'name' => "Powell River"]);
-        DB::table('pecsf_regions')->insert(['id' => 22,'name' => "Haida Gwaii"]);
+        DB::table('pecsf_regions')->insert(['id' => 21,'name' => "qathet"]);
+        DB::table('pecsf_regions')->insert(['id' => 22,'name' => "North Coast"]);
         DB::table('pecsf_regions')->insert(['id' => 23,'name' => "Squamish-Lillooet"]);
         DB::table('pecsf_regions')->insert(['id' => 24,'name' => "Stikine"]);
         DB::table('pecsf_regions')->insert(['id' => 25,'name' => "Sunshine Coast"]);
         DB::table('pecsf_regions')->insert(['id' => 26,'name' => "Thompson-Nicola"]);
-        DB::table('pecsf_regions')->insert(['id' => 27,'name' => "Vancouver"]);
-        DB::table('pecsf_regions')->insert(['id' => 28,'name' => "Victoria"]);
+        DB::table('pecsf_regions')->insert(['id' => 27,'name' => "Metro Vancouver"]);
+        DB::table('pecsf_regions')->insert(['id' => 28,'name' => "Capital / Victoria"]);
+        DB::table('pecsf_regions')->insert(['id' => 30,'name' => "Strathcona"]);
 
     }
 }
