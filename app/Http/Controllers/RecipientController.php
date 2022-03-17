e8da678b1ac58d29cd6b06ba8d5b6d23a8316a4c
spencercrose <spencer.rose@gov.bc.ca>
2022-03-17 16:52:48 -0700

    Updates to user and recipient controllers.

--- app/Http/Controllers/RecipientController.php
+++ app/Http/Controllers/RecipientController.php
@@ -27,7 +27,7 @@ class RecipientController extends Controller
   {
 
     // $this->authorize('viewAny', Recipient::class);
-    $user = $request->user('web');
+    $user = $request->user();
 
     Log::info('Manage Recipients', array(
       'user' => $user
