c4f877fee75d9eaf1169728bd59c2e125d08a0cf
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-28 08:48:51 -0700

    Added a test email to figure out wtf is happening with formatting.

---
+++ resources/views/emails/testEmail.blade.php
@@ -0,0 +1,10 @@
+THIS IS AN AUTOMATED EMAIL. NO RESPONSE IS REQUIRED.\n
+\n
+You have successfully registered for a year Long Service Award. Congratulations on reaching this milestone in your career!\n
+\n
+An email has been sent to your supervisor so they are aware of your registration.\n
+\n
+Find details about the program on the Long Service Awards website.\n
+\n
+Long Service Awards team\n
+
