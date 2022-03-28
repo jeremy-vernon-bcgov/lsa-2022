59bf04e0376af8f0a121b6777a1307fe3b969593
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2022-03-28 09:52:36 -0700

    Updated all email templates to HTML.

--- resources/views/emails/recipientNoCeremonyConfirm.blade.php
+++ resources/views/emails/recipientNoCeremonyConfirm.blade.php
@@ -1,10 +1,22 @@
-THIS IS AN AUTOMATED EMAIL. NO RESPONSE IS REQUIRED.
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
 
-You have successfully registered for a {{$recipient->milestones}} year Long Service Award. Congratulations on reaching this milestone in your career!
+<strong>THIS IS AN AUTOMATED EMAIL. NO RESPONSE IS REQUIRED.</strong>
 
-An email has been sent to your supervisor so they are aware of your registration.
+<p>You have successfully registered for a {{$recipient->milestones}} year Long Service Award. Congratulations on reaching this milestone in your career!</p>
 
-Find details about the program on the Long Service Awards website.
+<p>An email has been sent to your supervisor so they are aware of your registration.</p>
 
-Long Service Awards team
+<p>Find details about the program on the Long Service Awards website.</p>
+
+<p>Long Service Awards team</p>
+
+</body>
+</html>
 
--- resources/views/emails/recipientRegistrationConfirm.blade.php
+++ resources/views/emails/recipientRegistrationConfirm.blade.php
@@ -1,24 +1,36 @@
-THIS IS AN AUTOMATED EMAIL. NO RESPONSE IS REQUIRED.
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
 
-You have successfully registered for a {{$recipient->milestones}} year Long Service Award. Congratulations on reaching this milestone in your career!
+<p><strong>THIS IS AN AUTOMATED EMAIL. NO RESPONSE IS REQUIRED.</strong></p>
 
-An email has been sent to your supervisor so they are aware of your registration and can support you to attend your award ceremony.
+<p>You have successfully registered for a {{$recipient->milestones}} year Long Service Award. Congratulations on reaching this milestone in your career!</p>
 
-Find details about the program including the ceremony, travel policies and more on the Long Service Awards website.
+<p>An email has been sent to your supervisor so they are aware of your registration and can support you to attend your award ceremony.</p>
 
-Ceremony dates
+<p>Find details about the program including the ceremony, travel policies and more on the Long Service Awards website.</p>
 
-We are following the Provincial Health Officer’s directives regarding events and travel. We will provide any updates to registered recipients over the summer and update the Long Service Awards website.
+<p><strong>Ceremony dates</strong></p>
 
-Deferring attendance
+<p>We are following the Provincial Health Officer’s directives regarding events and travel. We will provide any updates to registered recipients over the summer and update the Long Service Awards website.</p>
 
-If you have registered but are unable to attend your ceremony, you may defer for one year and attend a ceremony in 2023. Please RSVP your regrets and re-register for the Long Service Awards next year. Arrangements will be made to have your award presented to you towards the end of the year.
+<p><strong>Deferring attendance</strong></p>
 
-Travel
+<p>If you have registered but are unable to attend your ceremony, you may defer for one year and attend a ceremony in 2023. Please RSVP your regrets and re-register for the Long Service Awards next year. Arrangements will be made to have your award presented to you towards the end of the year.</p>
 
-Please connect with your Long Service Award contact to confirm your organization’s travel process. You can also find information about the travel policy and reimbursement process on the Long Service Awards website.
+<p><strong>Travel</strong></p>
 
-We hope to see you at the ceremony this fall.
+<p>Please connect with your Long Service Award contact to confirm your organization’s travel process. You can also find information about the travel policy and reimbursement process on the Long Service Awards website.</p>
 
-Long Service Awards team
+<p>We hope to see you at the ceremony this fall.</p>
+
+<p>Long Service Awards team</p>
+
+</body>
+</html>
 
