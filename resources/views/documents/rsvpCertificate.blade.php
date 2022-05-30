11bebe9b86b8833c23479632c9b92c6c826cf3c7
spencercrose <spencer.rose@gov.bc.ca>
2022-05-30 14:25:13 -0700

    Update RSVP confirmation certificate PDF.

--- resources/views/documents/rsvpCertificate.blade.php
+++ resources/views/documents/rsvpCertificate.blade.php
@@ -30,7 +30,7 @@
 
   body {
     margin: 0px;
-    background-image: url({{ storage_path('app/public/images/border-background.png') }});
+    background-image: url('https://longserviceawards.gww.gov.bc.ca/wp-content/uploads/2022/05/certificate-border-background.png');
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
@@ -113,7 +113,7 @@
   <div class="certificate-body">
 
     <div class="coat-of-arms">
-      <img class="coat-of-arms" src="{{ storage_path('app/public/images/coat-of-arms.png') }}">
+      <img class="coat-of-arms" src="https://longserviceawards.gww.gov.bc.ca/wp-content/uploads/2022/05/coat-of-arms.png">
     </div>
     <div class="template-text">
 
