2cb8a743cb8fef4d31407c08ce122e7e78bae0ce
spencercrose <spencer.rose@gov.bc.ca>
2022-06-29 13:57:29 -0700

    Update email template graphics.

--- resources/views/documents/rsvpCertificate.blade.php
+++ resources/views/documents/rsvpCertificate.blade.php
@@ -31,7 +31,7 @@
 
   body {
     margin: 0px;
-    background-image: url('https://longserviceawards.gww.gov.bc.ca/rsvp/assets/certificate-background.jpg');
+    background-image: url('https://longserviceawards.gww.gov.bc.ca/wp-content/uploads/2022/06/lsa_certificate-bg.png');
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
@@ -112,7 +112,7 @@
   <div class="certificate-body">
 
     <div class="coat-of-arms">
-      <img class="coat-of-arms" src="https://longserviceawards.gww.gov.bc.ca/rsvp/assets/coat-of-arms.png">
+      <img class="coat-of-arms" src="https://longserviceawards.gww.gov.bc.ca/wp-content/uploads/2022/06/coat-of-arms-web.png">
     </div>
     <div class="template-text">
 
